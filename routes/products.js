0544cdb611f125041459d6e1d91718d10030c1f0
tranttxuan <>
2021-04-02 01:25:08 +0200

    modify

--- routes/products.js
+++ routes/products.js
@@ -5,10 +5,10 @@ const router = express.Router();
 router.get("/", (req, res, next) => {
     const catRequest = req.query.cat;
     const query = catRequest ? { category: catRequest } : {};
-    console.log(query)
+    // console.log(query)
     Product.find(query)
         .then(data => {
-            console.log(data)
+            // console.log(data)
             res.status(200).json(data)
         })
         .catch(err => res.status(500).json({ message: "not found product" }))
